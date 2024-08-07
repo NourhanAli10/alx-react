6e29c7aae405140aba589758ee5b4c56c07b4abd
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-08-08 00:04:55 +0300

    a

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,21 +1,21 @@
-import React from "react";
-import propTypes from "prop-types";
-import "./CourseList.css";
+import React from 'react';
+import propTypes from 'prop-types';
+import './CourseList.css';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
   // props:
   // - isHeader: bool, default: false
   // - textFirstCell: string, required
   // - textSecondCell: string, default: null
-  const row_background_color = { backgroundColor: "#f5f5f5ab" };
-  const header_row_background_color = { backgroundColor: "#deb5b545" };
+  const row_background_color = { backgroundColor: '#f5f5f5ab' };
+  const header_row_background_color = { backgroundColor: '#deb5b545' };
   let node;
   let style;
 
   if (isHeader) {
     style = header_row_background_color;
     if (!textSecondCell) {
-      node = <th colSpan="2">{textFirstCell}</th>;
+      node = <th colSpan='2'>{textFirstCell}</th>;
     } else {
       node = (
         <React.Fragment>
